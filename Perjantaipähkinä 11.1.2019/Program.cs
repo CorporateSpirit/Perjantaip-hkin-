33dc5b157ec0108dcbc942a64118ac6b4cda353d
Sami Elmroos <sami.elmroos@corporatespirit.fi>
2019-01-18 09:32:33 +0200

    Add project files.

---
+++ Perjantaipähkinä 11.1.2019/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perjantaipähkinä_11._1._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.ReadKey();
+        }
+    }
+}
